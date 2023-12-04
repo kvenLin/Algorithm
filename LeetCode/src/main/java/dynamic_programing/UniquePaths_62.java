2e3fe8a51f344e43f489e64ae1e7bd9aa1f6a21c
chailinfeng <chailinfeng@gm-robot.com>
2023-12-04 22:55:47 +0800

    动态规划62

--- LeetCode/src/main/java/dynamic_programing/UniquePaths_62.java
+++ LeetCode/src/main/java/dynamic_programing/UniquePaths_62.java
@@ -10,49 +10,22 @@ package dynamic_programing;
  */
 
 public class UniquePaths_62 {
-    public int f[][];
+    public int dp[][];
 
     public int uniquePaths(int m, int n) {
-        f = new int[m + 1][n + 1];
-        return dp2(m, n);
-    }
-
-    public int dp(int m, int n) {
-        if (m <= 1) {
-            return 1;
-        }
-        if (n <= 1) {
-            return 1;
-        }
-        if (f[m][n] != 0) {
-            return f[m][n];
-        }
-        int res = dp(m - 1, n) + dp(m, n - 1);
-        f[m][n] = res;
-        return res;
-    }
-
-    //第二种解法, 不使用递归的方式
-    public int dp2(int m, int n) {
-        int[][] f = new int[m][n];
-        f[0][0] = 1;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i > 0 && j > 0) {
-                    f[i][j] = f[i - 1][j] + f[i][j - 1];
-                } else if (i > 0) {
-                    f[i][j] = f[i - 1][j];
-                } else if (j > 0) {
-                    f[i][j] = f[i][j - 1];
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }else if (i > 0) {
+                    dp[i][j] = dp[i - 1][j];
+                }else if (j > 0) {
+                    dp[i][j] = dp[i][j - 1];
                 }
             }
         }
-        return f[m - 1][n - 1];
-    }
-
-    public static void main(String[] args) {
-        UniquePaths_62 ex = new UniquePaths_62();
-        int i = ex.uniquePaths(3, 7);
-        System.out.println(i);
+        return dp[m - 1][n -1];
     }
 }
